f0f264d279449487cc5b2c3090dc4bb09824cefc
Arseny Latyshev <arseny.latyshev@gmail.com>
2024-05-11 02:34:57 +0300

    Update DefaultSecurityConfig.java

--- PixelServer/AuthorizationServer/src/main/java/com/example/pixel/server/authorization/configuration/DefaultSecurityConfig.java
+++ PixelServer/AuthorizationServer/src/main/java/com/example/pixel/server/authorization/configuration/DefaultSecurityConfig.java
@@ -3,8 +3,6 @@ package com.example.pixel.server.authorization.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
